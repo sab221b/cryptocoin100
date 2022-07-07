b4c61242708cd0b2e925b5f93f159bb8c1b4f7e7
ravichandran <r.v.ravichandran@accenture.com>
2022-07-07 11:09:40 +0530

    [add]: callback, promise. async/await funct

--- controllers/crypto.js
+++ controllers/crypto.js
@@ -5,17 +5,44 @@ const mongoose = require('mongoose');
 const Cryptocoin = mongoose.model('Cryptocoin');
 
 module.exports = {
-    list(req, res) {
+    // fetching records with callback
+    // list(req, res) {
+    //     var query = req.query;
+    //     Cryptocoin.find(query, (err, response) => {
+    //         if (err) {
+    //             res.status(400).send('error fetching records: ' + err);
+    //         } else {
+    //             res.status(200).send(response);
+    //         }
+    //     }).limit(100);
+    // },
+
+    // fetching records with promise
+    // list(req, res) {
+    //     var query = req.query;
+    //     Cryptocoin.find(query).limit(100)
+    //     .then(result => {
+    //         res.status(200).send(result);
+    //     })
+    //     .catch(err => {
+    //         console.log('errored fetching records', err);
+    //         res.status(400).send(err);
+    //     })
+    // },
+
+    // fetching records with async/await function
+    async list(req, res) {
         var query = req.query;
-        Cryptocoin.find(query, (err, response) => {
-            if (err) {
-                res.status(400).send('error fetching records: ' + err);
-            } else {
-                res.status(200).send(response);
-            }
-        }).limit(100);
+        try {
+            const result = await Cryptocoin.find(query).limit(100);
+            res.status(200).send(result);
+        } catch (error) {
+            console.log('errored fetching records', error);
+            res.status(400).send(error);
+        }
     },
 
+    // fetching records with async/await function
     async bulkAdd(req, res) {
         let response = null;
         try {
